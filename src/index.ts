5a939d68300fdfa02979186ea31851e2a20d5fa4
GinoHsu <ginohsu16@gmail.com>
2023-04-04 21:44:38 +0800

    feat: 定義食物的 class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,40 @@
-import './style/index.less'
+import './style/index.less';
 
-console.log(123)
+// 定義food class
+class Food {
+  // 定義一個屬性表示食物所對應的元素
+  element: HTMLElement;
+
+  constructor() {
+    // 獲取葉面中的 food 元素並將其復職給 element
+    this.element = document.getElementById('food')!;
+  }
+
+  // 定義獲取食物 X 軸座標的方法
+  get X() {
+    return this.element.offsetLeft;
+  }
+
+  // 定義獲取食物 Y 軸座標的方法
+  get Y() {
+    return this.element.offsetTop;
+  }
+
+  // 修改食物位置的方法
+  change() {
+    // 生成一個隨機的位置
+    // 最小是0 最大是290
+    // 移動一次就是一格，一格的大小是 10
+    let top = Math.round(Math.random() * 29) * 10;
+    let left = Math.round(Math.random() * 29) * 10;
+
+    this.element.style.top = top + 'px';
+    this.element.style.left = left + 'px';
+  }
+}
+
+// TODO:測試
+const food = new Food();
+console.log(food.X, food.Y);
+food.change();
+console.log(food.X, food.Y);
